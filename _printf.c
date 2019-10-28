215daeee3c5373b69c7fd03c4047bc316dee8a78
Kathleen McKiernan <851@holbertonschool.com>
2019-10-28 21:19:40 +0000

    add basics of printf function with buffer

---
+++ _printf.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+/**
+ * _printf - produces formatted output
+ * @format: character string
+ * Return: number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+	va_list list;
+	int counter = 0;
+	int outcome = 0;
+	char specify;
+	char buffer[1024];
+
+	if (format == NULL)
+		return (-1);
+	va_start(list, format);
+	while (format && format[counter])
+		{
+		if (format[counter] == '%')
+		{
+			specify = format[counter + 1];
+		}
+		else
+		{
+		       	_puts(_strlen - 1(buffer));
+		}
+		counter += 1;
+		outcome += 1;
+		}
+	_puts(buffer);
+	va_end(list);
+	return(outcome);
+}
