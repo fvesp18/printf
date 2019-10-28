6665185922781c4257a059ff1d73729988a79198
Flavio Vilchez-Espinoza <fespholberton@gmail.com>
2019-10-28 19:34:10 +0000

    Prints string

---
+++ _puts.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * _puts - will print the string *str
+ * @str: location of str
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	int begin = 0;
+
+	while (str[begin] != '\0')
+	{
+		_putchar(str[begin]);
+		begin++;
+	}
+	_putchar('\n');
+}
