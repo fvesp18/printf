2abca5b6e3822b8ad55a638544f253d3797ad6d0
Kathleen McKiernan <851@holbertonschool.com>
2019-10-29 22:18:16 +0000

    add prototypes for new funct

--- holberton.h
+++ holberton.h
@@ -11,5 +11,8 @@ int _puts(char *str);
 int _print_di(int di);
 int _strlen(char *s);
 int switch_c(char, va_list);
+int print_hex(unsigned int n);
+void print_rev(char *s);
+void strtobin(unsigned int toconvert, unsigned int *ones);
 
 #endif
