6ae0a14ec405876b17215415503126ade9c6e959
Flavio Vilchez-Espinoza <fespholberton@gmail.com>
2019-10-29 21:41:44 +0000

    Update prototypes

--- holberton.h
+++ holberton.h
@@ -4,6 +4,8 @@
 #include <stdlib.h>
 #include <stdarg.h>
 #include <unistd.h>
+#include <strings.h>
+#include<limits.h>
 
 int _putchar(char c);
 int _printf(const char *format, ...);
@@ -11,5 +13,6 @@ int _puts(char *str);
 int _print_di(int di);
 int _strlen(char *s);
 int switch_c(char, va_list);
+char *itoa(int incoming, char* buff, int rad);
 
 #endif
