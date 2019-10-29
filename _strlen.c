c9cfc1f52aaaa24e7b466116b5096b2c14b65aaa
Flavio Vilchez-Espinoza <fespholberton@gmail.com>
2019-10-29 18:34:57 +0000

    Finds lenght of string

---
+++ _strlen.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _strlen - print length of string
+ * @s: string
+ * Return: void
+ */
+
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while (s[count] != '\0')
+	{
+
+		count++;
+
+	}
+	return (count);
+}
