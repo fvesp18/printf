d7464cb52c431b2220709aa189deac54f5d6340a
Kathleen McKiernan <851@holbertonschool.com>
2019-10-29 00:29:01 +0000

    add flag check

---
+++ flags.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+/**
+ * zeroflag - handle zero flag
+ * @flag: flag character being checked
+ * Return: flag if not cleared
+ */
+int zeroflag(int flag);
+{
+	if (flag == '0')
+	{
+		return (0);
+	}
+	else
+		return (1);
+}
+
+/**
+ * minusflag - left-justify within field
+ * @flag: flag character being checked
+ * Return: left-justify if cleared
+ */
+int minusflag(int flag);
+{
+	if (flag == '-')
+	{
+		return (0);
+	}
+	else
+		return (1);
+}
